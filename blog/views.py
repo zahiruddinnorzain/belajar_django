4c1f45ea57abcac64aa82ea79fbc277a43d907cd
zahiruddin <kepaknaga@gmail.com>
2020-02-16 13:42:55 +0800

    template takde base

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'CoreyMS',
+		'title': 'Blog Post 1',
+		'content': 'First post boys',
+		'date_posted': 'Ogos 27, 2019'
+	},
+	{
+		'author': 'Zahir',
+		'title': 'Blog Post 2',
+		'content': 'Second post na boys',
+		'date_posted': 'Ogos 29, 2019'
+	}
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
