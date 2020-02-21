1d5cb17ea81312655a982003f760823606e1afd8
zahiruddinnorzain <kepaknaga@gmail.com>
2020-02-21 11:52:18 +0800

    add form tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('borang/', views.get_name, name='blog-get_name'),
+    path('contact/', views.get_contact, name='blog-get_contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
 from .models import Post
 
+from .forms import NameForm, ContactForm
+from django.core.mail import send_mail
+
+def get_contact(request):
+    # if this is a POST request we need to process the form data
+	if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+		form = ContactForm(request.POST)
+        # check whether it's valid:
+		if form.is_valid():
+			subject = form.cleaned_data['subject']
+			message = form.cleaned_data['message']
+			sender = form.cleaned_data['sender']
+			cc_myself = form.cleaned_data['cc_myself']
+
+			recipients = ['info@example.com']
+			if cc_myself:
+				recipients.append(sender)
+
+		send_mail(subject, message, sender, recipients)
+		return HttpResponseRedirect('/thanks/')
+	else:
+		form = ContactForm()
+
+	return render(request, 'blog/name.html', {'form': form})
+	
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'blog/name.html', {'form': form})
+
+
 '''
 posts = [
 	{
